5c7cfe34fec0220661d15849f39396cdeb7998d5
inayatuizzati <159796022+inayatuizzati@users.noreply.github.com>
2024-06-18 18:33:21 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+  $(function () {
+    var flame = $('#flame');
+    var txt = $('h1');
+
+    flame.on({
+      click: function () {
+        flame.removeClass('burn').addClass('puff');
+        $('.smoke').each(function () {
+          $(this).addClass('puff-bubble');
+        });
+        $('#glow').remove();
+        txt.hide().html('it will come true...once again happy birthday Mohammad Rivky taufani, i love you from deep inside my heart❤︎').delay(750).fadeIn(300);
+        $('#candle').animate(
+          {
+            opacity: '.5',
+          },
+          100
+        );
+      },
+    });
+  });
+});
